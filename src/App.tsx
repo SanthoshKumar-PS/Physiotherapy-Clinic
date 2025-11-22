5f4d5a5e5a0797eecbc31677e784e177227db8da
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2025-11-22 10:55:33 +0530

    All Default Files to publish

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-
+import About from "./components/About"
+import Contact from "./components/Contact"
+import FAQ from "./components/FAQ"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import HowItWorks from "./components/HowItWorks"
+import Services from "./components/Services"
+import Testimonials from "./components/Testimonials"
 
 function App() {
 
   return (
     <div className='min-h-screen'>
-      Gayathri Physiotherapy Clinic
-
+      <Hero />
+      <About />
+      <Services />
+      <HowItWorks />
+      <Testimonials />
+      <Contact />
+      <FAQ />
+      <Footer />
     </div>
   )
 }
